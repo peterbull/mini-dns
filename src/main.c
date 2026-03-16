7029bce579086187352f16eaee959fd3c35191a4
peter bull <17122363+peterbull@users.noreply.github.com>
2026-03-16 05:28:57 -0400

    handle id and query

--- src/main.c
+++ src/main.c
@@ -1,11 +1,37 @@
 #include <arpa/inet.h>
+#include <assert.h>
 #include <netinet/in.h>
+#include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/_types/_socklen_t.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <stdbool.h>
+
+static_assert(true, "header to fix bug causing clangd pragma warning");
+
+#pragma pack(push, 1)
+typedef struct dns_header {
+  uint16_t id;
+  uint16_t flags;
+} dns_header_t;
+#pragma pack(pop)
+
+void parse_header(char *buffer) {
+  dns_header_t *header = (dns_header_t *)buffer;
+  uint16_t id = ntohs(header->id);
+  uint16_t flags = ntohs(header->flags);
+  uint8_t qr = (flags >> 15) & 0x1; 
+  printf("id: %d\n", id);
+  printf("qr: %d\n", qr);
+}
+
+void sig_handler(int signum) {
+  printf("\nCaught signal, exiting, %d", signum);
+  exit(0);
+}
 
 int main() {
   char *ip = "127.0.0.1";
@@ -28,13 +54,20 @@ int main() {
     perror("[-]bind error");
     exit(1);
   }
-  memset(buffer, '\0', 1024);
-  addr_size = sizeof(client_addr);
-  recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, &addr_size);
-  
-  memset(buffer, '\0', 1024);
-  strcpy(buffer, "dns server running");
-  sendto(sockfd, buffer,1024, 0 , (struct sockaddr*)&client_addr, sizeof(client_addr));
-  printf("[+]data send: %s\n ", buffer);
+
+  signal(SIGINT, sig_handler);
+
+  while (1) {
+    memset(buffer, '\0', 1024);
+    addr_size = sizeof(client_addr);
+    recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *)&client_addr,
+             &addr_size);
+    parse_header(buffer);
+    memset(buffer, '\0', 1024);
+    strcpy(buffer, "dns server running");
+    sendto(sockfd, buffer, 1024, 0, (struct sockaddr *)&client_addr,
+           sizeof(client_addr));
+    printf("[+]data send: %s\n ", buffer);
+  }
   return 0;
 }
